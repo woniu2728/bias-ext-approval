4858aabb73201429f8c8911fd6bff1d9e5deda6b
woniu <2463396298@qq.com>
2026-06-24 12:14:07 +0800

    Add apps.py + django_migrations

---
+++ bias_ext_approval/backend/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+class ApprovalExtensionConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "bias_ext_approval.backend"
+    label = "approval"
